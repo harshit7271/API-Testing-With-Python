9dcacef6a6178ad3711b3bcd84816106c45713af
Harshit Singh <harshitsingh05893312@gmail.com>
2026-06-02 10:23:43 +0530

    Updated

--- API Testing with Request Module/test_pytest.py
+++ API Testing with Request Module/test_pytest.py
@@ -1,6 +1,20 @@
-class TestClass:
+"""class TestClass:
     def testMethod(self):
         print("This is method 1")
 
     def testMethod2(self):
         print("This is method 2")
+        
+"""
+
+
+def test_open_app():
+    print("APP Opened")
+
+
+def test_login():
+    print("Login successful")
+
+
+def test_dashboard():
+    print("dashboard Loaded")
