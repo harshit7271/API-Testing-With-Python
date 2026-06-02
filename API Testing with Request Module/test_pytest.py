71a85dc643eabe761dba583df6cace9bfbc26202
Harshit Singh <harshitsingh05893312@gmail.com>
2026-06-02 13:43:17 +0530

    Updated

--- API Testing with Request Module/test_pytest.py
+++ API Testing with Request Module/test_pytest.py
@@ -9,16 +9,19 @@
 import pytest
 
 
+@pytest.mark.dependency()
 @pytest.mark.order(1)
 def test_open_app():
     print("APP Opened")
 
 
+@pytest.mark.dependency(depends=["test_login"])
 @pytest.mark.order(3)
 def test_dashboard():
     print("dashboard Loaded")
 
 
+@pytest.mark.dependency(depends=["test_open_app"])
 @pytest.mark.order(2)
 def test_login():
     print("Login successful")
